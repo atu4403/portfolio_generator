d719a8b048358742955d01db51bb1b2f3819c44a
atu4403 <73111778+atu4403@users.noreply.github.com>
2022-01-15 13:00:07 +0900

    🧹 refactor: templateを文字列からの読み込みに変更

--- src/portfolio_generator/templates.py
+++ src/portfolio_generator/templates.py
@@ -1,5 +1,6 @@
 default_template = """# {{ user }}
 
+{%- if github %}
 ## github
 
 {% for item in github %}
@@ -7,17 +8,20 @@ default_template = """# {{ user }}
 - [{{ item.name}}]({{ item.html_url }}): {{ item.description}} ({{ item.updated_at|to('+09:00')|fmt('YYYY-MM-DD HH:mm:ss')}})
 {%- endif %}
 {%- endfor %}
+{%- endif %}
 
+{%- if zenn %}
 ## zenn({{ zenn.entries|length }} items)
 
 {% for item in zenn.entries %}
 - [{{ item.title}}]({{ item.link }}): {{ item.published_parsed|fmt('YYYY-MM-DD HH:mm:ss')}}
 {%- endfor %}
+{%- endif %}
 
-{% if qiita %}
-## qiita({{ qiita|length }} items)
+{%- if qiita %}
+## qiita
 
-{%- for item in qiita %}
+{% for item in qiita %}
 - [{{ item.title}}]({{ item.url }}): {{ item.created_at|fmt('YYYY-MM-DD HH:mm:ss')}}
 {%- endfor %}
 {%- endif %}
